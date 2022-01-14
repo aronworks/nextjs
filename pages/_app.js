68242534f337a80679348d1431de2dee35217329
Vinoth <vinoth@Vinoths-MacBook-Pro.local>
2022-01-14 21:59:06 +0530

    add gtag

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
-import '../styles/globals.css'
+import { useRouter } from "next/router";
+
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+
+  const handleRouteChange = (url) => {
+    window.gtag("config", "G-Q2W4FS39HX", {
+      page_path: url,
+    });
+  };
+
+  useEffect(() => {
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
--- pages/_document.js
+++ pages/_document.js
@@ -23,42 +23,6 @@ class MyDocument extends Document {
             `,
             }}
           />
-
-          <Script
-            dangerouslySetInnerHTML={{
-              __html: `
-                var $zoho = $zoho || {};
-                $zoho.salesiq = $zoho.salesiq || {
-                widgetcode: "2c0df8d00ad4b1f384a18fad43a284d9974be180e82b8b6600b3863c340d9ccbf036c7fbb3e2ecd5d6845f394185c368",
-                values: {},
-                ready: function() {}
-                };
-                var d = document;
-                s = d.createElement("script");
-                s.type = "text/javascript";
-                s.id = "zsiqscript";
-                s.defer = true;
-                s.src = "https://salesiq.zoho.com/widget";
-                t = d.getElementsByTagName("script")[0];
-                t.parentNode.insertBefore(s, t);
-                d.write("<div id='zsiqwidget'></div>");
-
-                $zoho.salesiq.ready = function(embedinfo) {
-                $zoho.salesiq.floatwindow.fields({
-                    "online": "",
-                    "offline": "email,contactnumber"
-                });
-                $zoho.salesiq.chat.offline(function() {
-                    $zoho.salesiq.customfield.add({
-                    "name": "_default.email",
-                    "hint": "Email Address",
-                    "required": "true"
-                    });
-                });
-                }
-            `,
-            }}
-          />
         </Head>
         <body>
           <Main />
